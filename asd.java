8861c0e4cf1e696282b657a178197ac3ef5848ee
phenix235 <31757345+phenix235@users.noreply.github.com>
2017-09-08 15:41:05 +0900

    Delete asd.java

--- asd.java
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-package exam1;
-
-public class Hello {
-   public static void main (String[] arg){ 
-        system.out.println("Hello Java"); 
-   }
->>>>>>> e08ed95466b8f957b606345034e02a4f0106f500
-}
