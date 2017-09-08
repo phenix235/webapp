25f9d06b5f8bb8efb1281b0bfad42ec63be4d245
phenix235 <31757345+phenix235@users.noreply.github.com>
2017-09-08 15:30:42 +0900

    Update asd.java

--- asd.java
+++ asd.java
@@ -5,12 +5,5 @@ public class Hello {
    public static void main (String[] arg){ 
         system.out.println("Hello Java"); 
    }
-=======
-package exam1;
-
-public class Hello {
-   public static void main (String[] arg){ 
-        system.out.println("Hello Java"); 
-   }
 >>>>>>> e08ed95466b8f957b606345034e02a4f0106f500
 }
